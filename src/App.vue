c06aff2f35b0a3b5ab9f856b517482ad6aafa67a
mgrisole <grisole.maxime@gmail.com>
2018-07-28 12:34:11 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-content>
+      <ModuleTimer/>
+      <ModuleTimer/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import ModuleTimer from './components/ModuleTimer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ModuleTimer,
+  },
+  data() {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire',
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    };
+  },
+};
+</script>
---
+++ src/components/ModuleTimer.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container fluid class="moduleTimer">
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        {{timer}}
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: 'ModuleTimer',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      timer: moment().format('LTS'),
+    };
+  },
+  methods: {
+    update() {
+      setInterval( () => {
+        this.timer = moment().format('LTS');
+      }, 1000 );
+    }
+  },
+  mounted() {
+    this.update();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.moduleTimer {
+  height: calc(100vh/2);
+  background-color: #326273;
+  color: #EEEEEE;
+  display: flex;
+  align-items: center;
+}
+</style>
